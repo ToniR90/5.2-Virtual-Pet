cec8e3b1b3c0ec8cc716f950b86cceac5954527b
ToniR90 <romy90.t@gmail.com>
2025-07-16 22:05:05 +0200

    feat: First PetModel version finished

--- src/main/java/com/toni/virtualpel/model/Pet.java
+++ src/main/java/com/toni/virtualpel/model/Pet.java
@@ -19,22 +19,36 @@ public class Pet {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "pet_name")
+    @Column(name = "pet_name" , nullable = false)
     @NotBlank
     private String name;
 
     @NotBlank
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Variant variant;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Stage stage = Stage.EGG;
 
+    @Column(nullable = false)
     private int experience = 0;
-    private boolean readyToEvolve;
+
+    @Column(nullable = false)
     private int energy = 10;
+
+    @Column(nullable = false)
     private int happiness = 10;
+
+    @Column(nullable = false)
     private int hunger = 10;
+
+    @Column(nullable = false)
+    private boolean readyToEvolve;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id" , nullable = false)
     private User owner;
 
     public enum Variant {
