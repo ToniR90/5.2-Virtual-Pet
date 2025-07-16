f00bd68a52f6241b035303ceec3533b61e41b60d
ToniR90 <romy90.t@gmail.com>
2025-07-16 11:28:50 +0200

    First release and pom.xml updated

---
+++ src/test/java/com/toni/virtualpel/VirtualPetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toni.virtualpel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
