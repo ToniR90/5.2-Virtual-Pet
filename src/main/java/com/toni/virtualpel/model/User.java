991cb45554f3183c056affd922c395dc01621579
ToniR90 <romy90.t@gmail.com>
2025-07-17 10:30:43 +0200

    feat: general anottations added

--- src/main/java/com/toni/virtualpel/model/User.java
+++ src/main/java/com/toni/virtualpel/model/User.java
@@ -1,4 +1,17 @@
 package com.toni.virtualpel.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
 public class User {
 }
