90445e40117cb2c7bdfeac18830316f58563e24f
ToniR90 <romy90.t@gmail.com>
2025-07-19 23:26:24 +0200

    feat: AuditableEntity created

---
+++ src/main/java/com/toni/virtualpel/model/base/AuditableEntity.java
@@ -0,0 +1,23 @@
+package com.toni.virtualpel.model.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public abstract class AuditableEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
