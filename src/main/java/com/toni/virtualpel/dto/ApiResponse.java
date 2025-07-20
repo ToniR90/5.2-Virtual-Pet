1878728448fa26afa0e9a5dcf096089da2466862
ToniR90 <romy90.t@gmail.com>
2025-07-20 14:34:30 +0200

    feat: ApiResponse DTO

---
+++ src/main/java/com/toni/virtualpel/dto/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.toni.virtualpel.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+}
