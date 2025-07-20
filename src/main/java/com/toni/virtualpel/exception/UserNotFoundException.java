9230e137a4e782a1b6724970d3e11e547c536b57
ToniR90 <romy90.t@gmail.com>
2025-07-20 19:52:05 +0200

    feat: UserNotFoundException implementated

---
+++ src/main/java/com/toni/virtualpel/exception/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package com.toni.virtualpel.exception;
+
+public class UserNotFoundException extends VirtualPetException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(Long userId) {
+        super("User not found with id: " + userId);
+    }
+
+    public UserNotFoundException(String field, String value) {
+        super("User not found with " + field + ": " + value);
+    }
+}
