f82413ae34bdd8d982014bdceff79df286cbb954
ToniR90 <romy90.t@gmail.com>
2025-07-21 22:57:55 +0200

    update

--- src/test/java/com/toni/virtualpet/config/TestSecurityConfig.java
+++ src/test/java/com/toni/virtualpet/config/TestSecurityConfig.java
@@ -3,31 +3,31 @@ package com.toni.virtualpet.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @TestConfiguration
 @EnableWebSecurity
+@Profile("test")
 public class TestSecurityConfig {
 
-    @Bean
+    @Bean(name = "testPasswordEncoder")
     @Primary
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder testPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
+    @Bean(name = "testFilterChain")
     @Primary
     public SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authz -> authz
-                        .anyRequest().permitAll()
-                );
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
