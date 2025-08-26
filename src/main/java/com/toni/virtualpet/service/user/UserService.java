c091884a3975bfb22ec432f2d36b68220aa1cb30
ToniR90 <romy90.t@gmail.com>
2025-08-26 20:26:46 +0200

    feat: UserService

--- src/main/java/com/toni/virtualpet/service/user/UserService.java
+++ src/main/java/com/toni/virtualpet/service/user/UserService.java
@@ -2,10 +2,16 @@ package com.toni.virtualpet.service.user;
 
 import com.toni.virtualpet.exception.personalizedException.UserNotFoundException;
 import com.toni.virtualpet.model.user.User;
+import com.toni.virtualpet.model.user.enums.Role;
 import com.toni.virtualpet.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 @AllArgsConstructor
 public class UserService {
 
@@ -15,5 +21,21 @@ public class UserService {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public boolean existByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getUserByRole(Role role) {
+        return userRepository.findByRole(role);
     }
 }
